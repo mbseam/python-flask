08908fe1b657c59bacb02ac1dde66c5318b64205
mbseam <mbseam@gmail.com>
2021-12-25 23:44:09 +0300

    some stuff

---
+++ hello.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from markupsafe import escape
+from flask import url_for
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route("/<name>")
+def hellos(name):
+    return f"Hello, {escape(name)}!"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+@app.route('/hellox/')
+@app.route('/hellox/<name>')
+def hellox(name=None):
+    return render_template('hello.html', name=name)
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
